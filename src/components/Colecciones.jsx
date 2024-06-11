fdab1b6a441ebe46450264d05ad69a8d3b15f95e
GabrielAntonioRamosVelasco <ramos.velasco.gabriel.antonio@gmail.com>
2024-06-11 11:27:05 -0600

    Se modifica colecciones.jsx para conectarse al backend mediante axios

--- src/components/Colecciones.jsx
+++ src/components/Colecciones.jsx
@@ -1,41 +1,37 @@
-import { useEffect, useState } from 'react'
-import { Card } from "../components/Card"
-
-
+import { useEffect, useState } from 'react';
+import { Card } from "../components/Card";
+import axios from 'axios';
 
 export const Colecciones = () => {
+  const [productos, setProductos] = useState([]);
 
-  const [productos, setProductos] = useState([])
+  useEffect(() => {
+    fetchProductos();
+  }, []);
 
-    const fetchProductos = async () => {
-        const response = await fetch('https://fakestoreapi.com/products')
-        const data = await response.json()
-        console.log(data)
-        setProductos(data)
+  const fetchProductos = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/inventario");
+      console.log(response.data);
+      setProductos(response.data);
+    } catch (error) {
+      console.error('Error fetching products:', error);
     }
-
-    useEffect(() => {
-        fetchProductos()
-
-    }, [])
+  };
 
   return (
     <>
-    <h1>Compras: </h1>
-    <hr />
-
-    {productos.map(producto => (
+      <h1>Compras: </h1>
+      <hr />
+      {productos.map(producto => (
         <Card 
-        key={producto.id}
-        imagen={producto.image}
-        titulo={producto.title}
-        descripcion={producto.description}
-        precio={producto.price}
-        >
-
-        </Card>
-    ))}
-    
+          key={producto.id_producto}
+          imagen={producto.imagen}
+          titulo={producto.nombre}
+          descripcion={producto.descripcion}
+          precio={producto.precio}
+        />
+      ))}
     </>
-  )
+  );
 };
