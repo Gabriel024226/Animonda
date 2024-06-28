9d2a2837fa55b8e4603764b6a21d8f5322504f53
Gabriel024226 <sanchez.ortega.gabriel024226@gmail.com>
2024-06-28 01:16:19 -0600

    Creo ya quedo lo de contacto a ver si mañana pero la neta no creo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,48 @@
 import { Reel } from './reel';
-
+import '../Styles/home.css'
 export const Home = () => {
   
 
   return (
     <div className="Home">
-      <div className="der">
-        <h1>Prueba</h1>
-        <p>Pruevita deidad</p>
+      
+      <div className="ini">
+        <img className="imgizq" src='src/Assets/ichigo.gif' alt="ichigo"></img>
+        <div className="der">
+          <h1>Quienes somos?</h1>
+          <p>En AniModa, somos mas que una tienda de ropa. 
+            Somos una comunidad apasionada por el arte, la cultura y la moda inspirada en el vibrante mundo de la animacion japonesa. 
+            Nuestra mision es conectar a los fanaticos del anime con prendas unicas que reflejen su amor por sus personajes y series favoritas.</p>
+        </div>
       </div>
-    <Reel/>
+      <div className="ini">
+        <img className="imgder" src='src/Assets/gohan.gif' alt="gohandeidad"></img>
+        <div className="izq">
+          <h1>DiseNos Exclusivos</h1>
+          <p>Colaboramos con artistas talentosos para crear prendas unicas que no encontraras en ningun otro lugar. 
+            Cada diseno es una obra de arte que lleva el espiritu del anime directamente a tu guardarropa.</p>
+        </div>
+      </div>
+      <div className="ini">
+        <img className="imgizq" src='src/Assets/luffy4.gif' alt="luffy4"></img>
+        <div className="der">
+          <h1>Calidad y Confort</h1>
+          <p>Nos aseguramos de que cada prenda no solo se vea increible, sino que tambien sea comoda y duradera. 
+            Utilizamos materiales de alta calidad para que te sientas genial mientras representas a tus personajes favoritos.</p>
+        </div>
+      </div>
+      <div className="ini">
+        <div className="izq">
+          <h1>Pasion por el Anime</h1>
+          <p>Cada miembro de nuestro equipo es un verdadero fanatico del anime. 
+            Esto nos permite entender mejor tus gustos y necesidades, y nos motiva a ofrecer productos que realmente amaras.</p>
+        </div>
+      </div>
+      <div className="titulin">
+        <h1>Nuestros Disenos</h1>
+      </div>
+      
+      <Reel/>
     </div>
   );
 };
